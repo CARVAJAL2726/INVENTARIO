caf97b981757ab67cd31f09c6da1ff2cf7cb4c8c
CARVAJAL2726 <carvajalfelipeandres26@gmail.com>
2024-09-24 14:55:37 -0500

    se agrego la base de datos

--- app.py
+++ app.py
@@ -13,3 +13,10 @@ app.config['MYSQL_PASSWORD'] = ''
 app.config['MYSQL_PORT'] = 3306
 app.config['MYSQL_DB'] = 'gestion_inventarios'
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+
+
+mysql.init_app(app)
+
+@app.route('/')
+def productos():
+    return "hola"
