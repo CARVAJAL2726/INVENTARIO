6ce79374b5fee779e160d3868bd908b26a619726
CARVAJAL2726 <carvajalfelipeandres26@gmail.com>
2024-09-24 13:27:44 -0500

    se agregó el app.py

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, redirect, render_template, request
+from flask import *
+from flask_mysqldb import MySQL
+
+
+
+app = Flask(__name__)
+
+mysql = MySQL()
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_PORT'] = 3306
+app.config['MYSQL_DB'] = 'gestion_inventarios'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
